8ef6feb2c4e89ad926b17feb6bd0330783798d42
luckykeks <luckykeks@gmail.com>
2018-08-20 11:29:44 +0300

    page 404 and server check modified

--- httpServer.js
+++ httpServer.js
@@ -3,11 +3,18 @@ const shell = require('shelljs');
 
 
 const server = http.createServer(function (req, res) {
-	if(req.url == '/redeploy'){
+	
+	if(req.url == '/redeploy' && req.method == 'POST'){
 		shell.exec('script.bat');
+		console.log('' + req.method);
 		console.log('well done baby');
+		res.writeHead(200);
+		
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('Page 404 not found');
 	}
-    res.writeHead(200);
+    
     res.end();
 }).listen(8080);
 
