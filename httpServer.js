6ddb7f3824c766babf51af6f84a2783d057a5c3d
luckykeks <luckykeks@gmail.com>
2018-08-26 19:50:39 +0300

    Response server v 1.2

--- httpServer.js
+++ httpServer.js
@@ -1,29 +1,33 @@
 const http = require('http');
 const shell = require('shelljs');
 const fs = require('fs');
-const fileStr = 'F:/HttpServer';
-const fileExtension = '.bat';
+const path = require('path');
+const Folder = './scripts';
 
 const server = http.createServer(function (req, res) {
 	
 	if(req.method == 'POST') {
-		try {
-		const stat = fs.lstatSync(fileStr+req.url+fileExtension);
-		if(stat.isFile){
-			let shellFileName = '".' + req.url  +'"';
-			shell.exec(shellFileName);
-			res.writeHead(200);
-			console.log("WELL DONE BABY");
-		}
-		} catch(err) {
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			res.write('Page 404 not found');
-		}
-
+		const cutUrlName = path.parse(req.url).name;
+			fs.readdir(Folder, (err, files) => {
+				let found = files.find(file => {
+					const cutFileName = file.toString().split('.').slice(0, -1).join('.');
+					if(cutFileName === cutUrlName){
+						return cutFileName;
+					}
+				});
+					if(found !== undefined){
+						let shellFileName = '".' + req.url  +'"';
+						shell.exec(shellFileName);
+						res.writeHead(200);
+					} else {
+						res.writeHead(404, {'Content-Type': 'text/html'});
+						res.write('Page 404 not found');
+					}
+			});
 	} else {
 		res.writeHead(404, {'Content-Type': 'text/html'});
 		res.write('Page 404 not found');
-	}
+	}	
     res.end();
 }).listen(8080);
 
